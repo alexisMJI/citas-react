7006d1d79844fd0e1b4a3bb8a4707236b6b3bd05
alexisMJI <alexismarin11@hotmail.es>
2022-03-21 18:23:46 -0300

    m2

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+
+const Paciente = () => {
+  return (
+    <div  className="m-5 bg-white shadow-md px-5 py-10 rounded-md">
+      <p className=" font-bold mb-3 text-gray-700 uppercase">
+        Nombre de Mascota:
+        <span className="font-normal normal-case"> Hook</span>
+      </p>
+
+      <p className=" font-bold mb-3 text-gray-700 uppercase">
+        Nombre del propietario:
+        <span className="font-normal normal-case"> Juan</span>
+      </p>
+
+      <p className=" font-bold mb-3 text-gray-700 uppercase">
+        E-Mail:
+        <span className="font-normal normal-case"> correoejemplo@dominio.com</span>
+      </p>
+
+      <p className=" font-bold mb-3 text-gray-700 uppercase">
+        Fecha del Alta:
+        <span className="font-normal normal-case"> 21 de Marzo de 2022</span>
+      </p>
+
+      <p className=" font-bold mb-3 text-gray-700 uppercase">
+        Observaciones:
+        <span className="font-normal normal-case"> Lorem ipsum doi fugiat, inventore a maiores eius sint exp iendis mollitia vel dolor, neque aut odit alias!</span>
+      </p>
+  </div>
+  )
+}
+
+export default Paciente
