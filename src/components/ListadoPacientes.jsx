e00233081ab5fd207ec15f5d26837830157318ed
alexisMJI <alexismarin11@hotmail.es>
2022-03-22 19:51:44 -0300

    m

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -15,8 +15,8 @@ export default function ListadoPacientes() {
         <Paciente />
         <Paciente />
         <Paciente />
-        <Paciente />
-
+        <Paciente  />
+        <Paciente  />
        
     </div>
   )
